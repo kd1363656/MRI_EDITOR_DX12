e5c6fbed89d6f98091fbb1a234eec2611df6961c
kd1363656 <kd1363656@st.kobedenshi.ac.jp>
2026-01-17 13:39:05 +0900

    最初にウィンドウクラスの作成

--- Source/PCH.h
+++ Source/PCH.h
@@ -1,1 +1,48 @@
 #pragma once
+
+//===============================================
+// 基本
+//===============================================
+#pragma comment(lib , "winmm.lib")
+#pragma comment(lib , "Rpcrt4.lib")
+
+#define NOMINMAX
+#include <Windows.h>
+#include <stdio.h>
+
+#include <wrl/client.h>
+
+//===============================================
+// "STL"
+//===============================================
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <array>
+#include <vector>
+#include <stack>
+#include <list>
+#include <iterator>
+#include <queue>
+#include <algorithm>
+#include <memory>
+#include <random>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <functional>
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <future>
+#include <filesystem>
+#include <source_location>
+#include <codecvt>
+
+//===============================================
+// 外部便利ライブラリ
+//===============================================
+
+#include "json.hpp"
+#include "strconv2.h"
