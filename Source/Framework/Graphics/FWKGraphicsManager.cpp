7145fabd73d26f6ad7855c4b57c4a5f334747529
kd1363656 <kd1363656@st.kobedenshi.ac.jp>
2026-01-19 10:46:51 +0900

    コマンドキュー、リスト、アロケータの作製完了

--- Source/Framework/Graphics/FWKGraphicsManager.cpp
+++ Source/Framework/Graphics/FWKGraphicsManager.cpp
@@ -16,6 +16,12 @@ bool FWK::Graphics::GraphicsManager::Init()
 		return false;
 	}
 
+	if (!CreateCommandObjects())
+	{
+		assert(false && "コマンド関連オブジェクトの作成に失敗しました。");
+		return false;
+	}
+
 	return true;
 }
 
@@ -24,13 +30,13 @@ bool FWK::Graphics::GraphicsManager::CreateFactory()
 	UINT l_flagsDXGI = 0U;
 
 #if defined (_DEBUG)
-	// "D3D12"のデバックレイヤーを有効化
-	EnableDebugLayer();
+	EnableDebugLayer();							// "D3D12"のデバックレイヤーを有効化
 	l_flagsDXGI |= DXGI_CREATE_FACTORY_DEBUG;	// "DXGI"関係のログ出力を可能にするフラグ
 #endif
 
-	// "DXGI"ファクトリーの作成 ("IDXGIFactoy7"使用)
-	HRESULT l_hr = CreateDXGIFactory2(l_flagsDXGI , IID_PPV_ARGS(&m_dxgiFactory));	// "CreateDXGIFactory2"はデバックモードかどうかを選べるだけ
+	// "DXGI"ファクトリーの作成 
+	// 第一引数 :デバックモードフラグを"On"にするか"Off"にするか 、第二引数 : 取得するインターフェースの"ID"と、結果を受け取るポインタ
+	HRESULT l_hr = CreateDXGIFactory2(l_flagsDXGI , IID_PPV_ARGS(&m_dxgiFactory));	// "CreateDXGIFactory2"はデバックモードかどうかを選べる
 
 	if (FAILED(l_hr))
 	{
@@ -49,9 +55,9 @@ bool FWK::Graphics::GraphicsManager::CreateDevice()
 		return false;
 	}
 
-	ComPtr<IDXGIAdapter4> l_adapter       = nullptr;
+	ComPtr<IDXGIAdapter4> l_adapter       = nullptr;	// アダプターの列挙に使用
 	bool				  l_deviceCreated = false;
-
+	
 	// フィーチャーレベルは"GPU"がサポートする機能の段階を示し、アプリケーションが利用できるグラフィックス機能の範囲を示す。
 	constexpr D3D_FEATURE_LEVEL l_featureLevels[] =
 	{
@@ -62,11 +68,13 @@ bool FWK::Graphics::GraphicsManager::CreateDevice()
 		D3D_FEATURE_LEVEL_11_0 , 
 	};
 
-	// 高パフォーマンス"GPU"優先でアダプターを列挙
-	// 使用するグラフィックスカードは一つだけ
+	// 高パフォーマンス"GPU"優先でアダプターを列挙、使用するグラフィックスカードは一つだけ
+	// 第一引数 : 列挙するアダプターのインデックス、第二引数 : アプリの"GPU"、第三引数 : 取得するインターフェースの"ID"と、結果を受け取るポインタ
 	for (UINT l_i = 0U; m_dxgiFactory->EnumAdapterByGpuPreference(l_i , DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE , IID_PPV_ARGS(&l_adapter)) != DXGI_ERROR_NOT_FOUND; ++l_i)
 	{
 		DXGI_ADAPTER_DESC3 l_desc;
+
+		// "GPU"アダプターの表際情報の取得に成功したかどうか
 		if (FAILED(l_adapter->GetDesc3(&l_desc))) { continue; }
 
 		// ソフトウェアアダプター("WARP")は後で処理
@@ -82,6 +90,7 @@ bool FWK::Graphics::GraphicsManager::CreateDevice()
 		// 一致するフィーチャーレベルを最高レベルからレベルを下げていって探索
 		for (const auto& l_level : l_featureLevels)
 		{
+			// 第一引数 : デバイスの作成に使用するグラフィックカードのポインター、第二引数 : デバイス作成を成功させるために必要な"D3D_FEATURE_LEVEL"、第三引数 : 取得するインターフェースの"ID"と、結果を受け取るポインタ
 			if (!SUCCEEDED(D3D12CreateDevice(l_adapter.Get() , l_level , IID_PPV_ARGS(&m_device)))) { continue; }
 			
 #if defined (_DEBUG)
@@ -98,6 +107,55 @@ bool FWK::Graphics::GraphicsManager::CreateDevice()
 
 	// どの"GPU"でも失敗した場合に使用
 	return l_deviceCreated;
+}
+bool FWK::Graphics::GraphicsManager::CreateCommandObjects()
+{
+	if (!m_device)
+	{
+		assert(false && "デバイスの作製ができていません。");
+		return false;
+	}
+
+	// コマンドアロケーターの作成(第一引数 : 作製するコマンドアロケーターの種類、第二引数 : 取得するインターフェースの"ID"と、結果を受け取るポインタ
+	auto l_hr = m_device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT , IID_PPV_ARGS(&m_commandAllocator));
+
+	// コマンドアロケータ作成に失敗したら"assert"
+	if (FAILED(l_hr))
+	{
+		assert(false && "コマンドアロケータの作成に失敗しました。");
+		return false;
+	}
+
+	// コマンドキュー作成
+	D3D12_COMMAND_QUEUE_DESC l_queueDesc = {};
+	l_queueDesc.Type     = D3D12_COMMAND_LIST_TYPE_DIRECT;		// どの種類のコマンドを渡すキューか
+	l_queueDesc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL; // キューの優先度を指定
+	l_queueDesc.Flags    = D3D12_COMMAND_QUEUE_FLAG_NONE;		// コマンドキューに対するオプション(現在格納しているフラグは"GPU"のタイムアウトを無効化)
+	l_queueDesc.NodeMask = k_singleGPUNodeMask;					// マルチアダプターを指定
+	
+	// 第一引数 : 作製するキューのパラメータ、第二引数 : 取得するインターフェースの"ID"と、結果を受け取るポインタ
+	l_hr = m_device->CreateCommandQueue(&l_queueDesc , IID_PPV_ARGS(&m_commandQueue));
+	if (FAILED(l_hr))
+	{
+		assert(false && "コマンドキューの作成に失敗しました。");
+		return false;
+	}
+
+	// コマンドリスト作成
+	l_hr = m_device->CreateCommandList1(0U							        ,	// 複数の"GPU"を使うかどうか
+										D3D12_COMMAND_LIST_TYPE_DIRECT      ,	// 作製するコマンドリストの種類
+										D3D12_COMMAND_LIST_FLAG_NONE        ,	// 作製フラグ
+									    IID_PPV_ARGS(&m_graphicsCommandList));	// 取得するインターフェースの"ID"と、結果を受け取るポインタ
+
+	if (FAILED(l_hr))
+	{
+		assert(false && "グラフィックスコマンドリストの作成に失敗しました。");
+		return false;
+	}
+
+	// 作製直後のコマンドリストを明示的に"Close"
+	m_graphicsCommandList->Close();
+	return true;
 }
 
 #if defined (_DEBUG)
--- Source/Framework/Graphics/FWKGraphicsManager.h
+++ Source/Framework/Graphics/FWKGraphicsManager.h
@@ -10,22 +10,29 @@ namespace FWK::Graphics
 	
 	private:
 
-		bool CreateFactory();
-		bool CreateDevice ();
-		
+		bool CreateFactory       ();
+		bool CreateDevice        ();
+		bool CreateCommandObjects();
+
 #if defined (_DEBUG)
 		void EnableDebugLayer() const;
 #endif
 
 #if defined (_DEBUG)
-		static constexpr const wchar_t* const k_debugSelectedGPUText = L"選択広報 GPU";
+		static constexpr const wchar_t* const k_debugSelectedGPUText = L"選択候補 GPU";
 		static constexpr const wchar_t* const k_debugSucceeded		 = L" -> デバイス作成に成功\n";
 		static constexpr const wchar_t* const k_lineBreak			 = L"\n";
 #endif
 
+		static constexpr UINT k_singleGPUNodeMask = 0U;	// 単一"GPU"を使用するノードマスク。マルチアダプター非対応。
+
 		ComPtr<ID3D12Device10>  m_device      = nullptr;	// "DirectX12"においてオブジェクトの生成などに使用
 		ComPtr<IDXGIFactory7>   m_dxgiFactory = nullptr;	// 使用するグラフィックカードの設定やスワップチェーンの作製に使用
 		ComPtr<IDXGISwapChain4> m_swapChain   = nullptr;	// ダブルバッファリングにおける描画バッファの切り替えに使用するスワップチェーン
+
+		ComPtr<ID3D12GraphicsCommandList7> m_graphicsCommandList = nullptr;
+		ComPtr<ID3D12CommandQueue>         m_commandQueue        = nullptr;
+		ComPtr<ID3D12CommandAllocator>     m_commandAllocator    = nullptr;
 
 		//===============================
 		// シングルトン
